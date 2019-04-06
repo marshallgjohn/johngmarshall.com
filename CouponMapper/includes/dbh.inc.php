10aef5c266b1a646dd450f3a32299d60674830dc
John-PC <johnmarsh19@hotmail.com>
2019-04-06 02:57:26 -0500

    Update dbh.inc.php
    Fixing database to match server not client

--- CouponMapper/includes/dbh.inc.php
+++ CouponMapper/includes/dbh.inc.php
@@ -1,8 +1,8 @@
 <?php
 
 $server = "localhost";
-$dbUser = "root";
-$dbPassword = "";
+$dbUser = "garrett";
+$dbPassword = "Luckysnickerscat1997";
 $dbName = "coupon_marker";
 
 $conn = mysqli_connect($server, $dbUser, $dbPassword,$dbName);
